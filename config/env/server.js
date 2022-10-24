66d6f4d2bd73dabd7357068777ac15bb84e39862
johndao1005 <john.d1729@gmail.com>
2022-10-25 09:59:27 +1300

    update code

--- config/env/server.js
+++ config/env/server.js
@@ -1,7 +1,8 @@
-module.exports = ({ env }) => ({
-  url: env("https://bee-beauty.herokuapp.com/"),
-  proxy: true,
-  app:{
-    keys:env.array('APP_KEYS',['q8zO4v1C7UvalgAFjwoyBg==,ngd7fP27Kku+uXziddlHHA==,4njcqfobHf/aFQ9q4pp0zg==,wmXJPtc3R125XXECWmM14g==']),
-  }
-});
+
+    module.exports = ({ env }) => ({
+      proxy: true,
+      url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
+      app: { 
+        keys: env.array('APP_KEYS')
+      },
+  });
